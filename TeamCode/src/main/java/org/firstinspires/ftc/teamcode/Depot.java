fa7ba8a3d5c5ac8f6979f0ac6f6de97ef76a6a4f
deanlange <langed@wdmcs.org>
2018-11-05 16:28:04 -0600

    detecting the minerals is working for depot but not crater

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Depot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Depot.java
@@ -27,42 +27,54 @@ public class Depot extends Team6340Controls {
 
         // Insert Autonomous Code Here
         //TODO Try adding timeout Ex: getMineralPosition(15)
-        String position = getMineralPosition();
+        String position = getMineralPosition(10); //wait 10 sec if you don't get the mineral position run default case
         telemetry.addData("Position from Depot: ",position);
 
         if (POSITION_CENTER.equals(position)) {                             //If the mineral position is Center
             lift(-.5, 16, 10);//Land
-            gyroDrive(DRIVE_SPEED, 60, -5, 10);    //Move forward 43 inches
+            gyroDrive(DRIVE_SPEED, 64, 0, 10);    //Move forward 43 inches
             trophy.setPosition(.7);                                         //drop trophy set servo .7/up
             gyroHold(TURN_SPEED, 0, 1);                      //stop for 1 sec
-            gyroTurn(TURN_SPEED, 215, 5);                    //turn 320 degrees right
-            gyroDrive(DRIVE_SPEED, 72, 215, 10);    //move forward 43 inches
+            gyroDrive(DRIVE_SPEED, -10,0, 5 );
+            gyroTurn(TURN_SPEED, -120, 5);                    //turn 320 degrees right
+            gyroDrive(DRIVE_SPEED, 67, -120, 10);    //move forward 43 inches
             trophy.setPosition(1.0);                                         //set servo to .40/down
 
         } else if (POSITION_LEFT.equals(position)) {                       //If the mineral position is Left
             lift(-.5, 16, 10);//Land
+            gyroDrive(DRIVE_SPEED, 4, 0, 4);         //drive forward 4 inches
             gyroTurn(TURN_SPEED, 20, 5);                    //turn Left for 20 degrees
-            gyroDrive(DRIVE_SPEED, 43, 20, 10);    //Move forward 43 inches
-            gyroTurn(TURN_SPEED, -40, 5);                     //turn right -45 degrees
+            gyroDrive(DRIVE_SPEED, 58, 20, 10);    //Move forward 58 inches
+            gyroTurn(TURN_SPEED, -40, 5);                     //turn right -40 degrees
             gyroDrive(DRIVE_SPEED, 20, -40, 10);    //Move forward 20 inches
             trophy.setPosition(.7);                                         //drop trophy set servo .7/up
             gyroHold(TURN_SPEED, -50, 1);                      //stop for 1 sec
-            gyroDrive(DRIVE_SPEED, -95, -50, 10);    //move backward 85 inches
+            gyroDrive(DRIVE_SPEED, -90, -50, 10);    //move backward 90 inches
            trophy.setPosition(1.0);
 
         } else if (POSITION_RIGHT.equals(position)) {                       //If the mineral position is Right
             lift(-.5, 16, 10);//Land
+            gyroDrive(DRIVE_SPEED, 4, 0, 4);         //drive forward 4 inches
             gyroTurn(TURN_SPEED, -20, 5);                    //turn Right for 20 degrees
             gyroDrive(DRIVE_SPEED, 53, -20, 10);    //Move forward 53 inches
             gyroTurn(TURN_SPEED, 45, 5);                     //turn left to 45 degrees
             gyroDrive(DRIVE_SPEED, 20, 45, 10);    //Move forward 20 inches
             trophy.setPosition(.7);                                         //drop trophy set servo .7/up
             gyroHold(TURN_SPEED, 50, 1);                      //stop for 1 sec
-            gyroDrive(DRIVE_SPEED, -95, 50, 10);    //move backward 85 inches
+            gyroDrive(DRIVE_SPEED, -95, 50, 10);    //move backward 95 inches
             trophy.setPosition(1.0);
-        } else {
+        } else {                                                               //default case
             telemetry.addData("No position from TensorFlow", "Run default program");
-            //TODO add default code here
+            //If we dont have position, assume LEFT position
+            lift(-.5, 16, 10);//Land
+            gyroDrive(DRIVE_SPEED, 4, 0, 4);         //drive forward 4 inches
+            gyroTurn(TURN_SPEED, 20, 5);                    //turn Left for 20 degrees
+            gyroDrive(DRIVE_SPEED, 58, 20, 10);    //Move forward 58 inches
+            gyroTurn(TURN_SPEED, -40, 5);                     //turn right -40 degrees
+            gyroDrive(DRIVE_SPEED, 20, -40, 10);    //Move forward 20 inches
+            trophy.setPosition(.7);                                         //drop trophy set servo .7/up
+            gyroHold(TURN_SPEED, -50, 1);                      //stop for 1 sec
+            gyroDrive(DRIVE_SPEED, -90, -50, 10);    //move backward 90 inches
         }
 
     }
